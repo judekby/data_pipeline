2d9c1cc8d69991155a156c333b2278bbf1403c29
judekabeya <judek9392@gmail.com>
2024-09-18 21:32:31 +0200

    update : fonctions

--- pipeline.py
+++ pipeline.py
@@ -1,62 +1,77 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
-def connect() :
+def connect_db():
     dbName = os.getenv("dbName")
     dbUser = os.getenv("dbUser")
     dbPassword = os.getenv("dbPassword")
     dbPort = os.getenv("dbPort")
     dbHost = os.getenv("dbHost")
-    conn = psycopg2.connect(host = dbHost,port = dbPort ,dbname =  dbName, user =  dbUser , password = dbPassword )
     
-    cursor = conn.cursor()
-    cursor.execute('select * from enseignants ; ')
-    enseignants_dict = []
-    
+    conn = psycopg2.connect(
+        host=dbHost, 
+        port=dbPort, 
+        dbname=dbName, 
+        user=dbUser, 
+        password=dbPassword
+    )
+    return conn
 
+def fetch_enseignants(cursor):
+    cursor.execute('SELECT * FROM enseignants;')
+    enseignants_dict = []
     for row in cursor:
         enseignant_dict = {
-         'Enseignant' :  row[1],
-         'Responsable' : row[3] 
+            'Enseignant': row[1],
+            'Responsable': row[3]
         }
         enseignants_dict.append(enseignant_dict)
-        
+    return enseignants_dict
+
+def fetch_eleves(cursor):
+    cursor.execute('SELECT * FROM eleves;')
     eleves_dict = []
-    cursor.execute('select * from eleves ;')
     for row in cursor:
         eleve_dict = {
-        'Eleve' :  row[1],
-        'Responsable' : row[3] 
+            'Eleve': row[1],
+            'Responsable': row[3]
         }
         eleves_dict.append(eleve_dict)
-        
-        
-    for enseignant in enseignants_dict:
+    return eleves_dict
+
+def associate_students_with_teachers(enseignants, eleves):
+    for enseignant in enseignants:
         enseignant['nb_associes'] = 0
 
-    for eleve in eleves_dict:
+    for eleve in eleves:
         eleve['associes_a'] = 'Non trouvé'
-        
-        for enseignant in enseignants_dict:
+        for enseignant in enseignants:
             if enseignant.get('Responsable') == eleve.get('Responsable'):
                 enseignant['nb_associes'] += 1
                 eleve['associes_a'] = enseignant.get('Enseignant')
 
-    for enseignant in enseignants_dict:
+def print_res(enseignants, eleves):
+    for enseignant in enseignants:
         print(f"L'enseignant : {enseignant.get('Enseignant')} a {enseignant.get('nb_associes')} élève(s) associé(s).")
 
-    for eleve in eleves_dict:
+    for eleve in eleves:
         print(f"L'élève : {eleve.get('Eleve')} est associé au prof : {eleve.get('associes_a')}")
-    
-    
-    print(enseignants_dict)
+
+def main():
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    enseignants = fetch_enseignants(cursor)
+    eleves = fetch_eleves(cursor)
+
+    associate_students_with_teachers(enseignants, eleves)
+    print_res(enseignants, eleves)
+
+    cursor.close()
+    conn.close()
+
 if __name__ == '__main__':
-    connect()
-    
-   
-
-    
-
-    
+    main()
